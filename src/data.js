77b616475872d20fea6d70e589bb702de34b75fb
Arthur Shmavonyan <arthurshmavonyan@gmail.com>
2023-03-02 00:01:04 +0400

    Add data for homepage slider

---
+++ src/data.js
@@ -0,0 +1,27 @@
+import wallpaper1 from "./assets/images/fashionable-woman-red-coat-black-hat-posing.jpg";
+import wallpaper0 from './assets/images/wp6595195.jpg'
+import wallpaper2 from './assets/images/38-384787_transparent-female-models-png-transparent-women-fashion-png.png'
+
+export const sliderItems = [
+        {
+            id: 1,
+            img: wallpaper0,
+            title: "SUMMER SALE",
+            desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+            bg: "f5fafd"
+        },
+        {
+            id: 2,
+            img: wallpaper1,
+            title: "AUTUMN COLLECTION",
+            desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+            bg: "fcf1ed"
+        },
+        {
+            id: 3,
+            img: wallpaper2,
+            title: "LOUNGEWEAR LOVE",
+            desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+            bg: "fbf0f4"
+        }
+    ]
