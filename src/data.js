8e3c656d860059c904f4baa24a35f2e9d826260d
Arthur Shmavonyan <arthurshmavonyan@gmail.com>
2023-03-05 02:41:45 +0400

    Changed data

--- src/data.js
+++ src/data.js
@@ -79,4 +79,20 @@ export const popularProducts = [
         id:6,
         img: product6
     },
+    {
+        id:7,
+        img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+        id:8,
+        img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+        id:9,
+        img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+        id:10,
+        img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    }
 ]
